69ab4eccb849e93a7e3e552f7640d06df077e78e
蜜瓜 <1176891456@qq.com>
2023-05-29 10:10:18 +0800

    first commit

---
+++ .vitepress/config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vitepress';
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: 'Vue3 Admin',
+  description: '一个开箱即用的Vue3后台管理前端框架',
+  base: '/vue3-admin-doc/',
+  themeConfig: {
+    logo: '/logo.svg',
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' },
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' },
+        ],
+      },
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' },
+    ],
+  },
+});
---
+++ index.md
@@ -0,0 +1,28 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "Vue3 Admin"
+  text: "一个开箱即用的Vue3后台管理前端框架"
+  # tagline: My great project tagline
+  actions:
+    - theme: brand
+      text: 快速开始
+      link: /markdown-examples
+    - theme: alt
+      text: 在线预览
+      link: https://vue3-ts-admin-brown.vercel.app
+
+features:
+ 
+  - title: 📦 开箱即用
+    details: 轻量快速，开箱即用
+  - title: 🔥 最新技术栈
+    details: 基于 Vue3、Vite、TypeScript 等最新技术栈
+  - title: 🌈 Tailwind CSS
+    details: 支持 Tailwind CSS 原子类样式
+  - title: 🔑 权限管理
+    details: 完善的权限管理
+---
+
