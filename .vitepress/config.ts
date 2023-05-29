edc81266d4bb61d4deab1f18728e6eccb3615e71
蜜瓜 <1176891456@qq.com>
2023-05-29 10:12:09 +0800

    fix:base

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from 'vitepress';
 export default defineConfig({
   title: 'Vue3 Admin',
   description: '一个开箱即用的Vue3后台管理前端框架',
-  base: '/vue3-admin-doc/',
   themeConfig: {
     logo: '/logo.svg',
     // https://vitepress.dev/reference/default-theme-config
